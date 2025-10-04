db70eb211de21b5a494ff4ad60151c5613701ba1
zahraa <ftoomrrt@gmail.com>
2025-10-04 20:04:37 +0200

    user

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from django.urls import path
+from users.views import loginview
 urlpatterns = [
-    # Auth (JWT)
-    path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/login", loginview.as_view(), name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .serializers import LoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class loginview(generics.CreateAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # توليد التوكنز مباشرة بعد التسجيل
+        refresh = RefreshToken.for_user(user)
+        data = {
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "name": user.name,
+                "phone": user.phone,
+                "role": user.role,
+            },
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
